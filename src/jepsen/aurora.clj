d96c16084a7d6cc2216b394fb48cc293ae4494f9
Jincheng Li <jens.vivre@gmail.com>
2015-12-04 13:48:16 -0500

    add build-aurora

--- src/jepsen/aurora.clj
+++ src/jepsen/aurora.clj
@@ -1,4 +1,21 @@
 (ns jepsen.aurora
   "Sets up Aurora"
+  (:require [jepsen [client :as client]
+                    [db :as db]
+                    [tests :as tests]
+                    [control :as c :refer [|]]
+                    [checker :as checker]
+                    [generator :as gen]
+                    [util :as util :refer [meh timeout]]
+                    [zookeeper :as zk]])
 )
+
+(defn build-aurora
+  ""
+  []
+  (c/exec "sudo apt-get update")
+  (c/exec "sudo apt-get install build-essential")
+  (c/exec "git clone http://git-wip-us.apache.org/repos/asf/aurora.git")
+  (c/exec "cd aurora")
+  (c/exec "./gradlew distZip"))
 
