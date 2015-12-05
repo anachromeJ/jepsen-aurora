d0b80f16b9524ac66a5cf02b70b2d7b2f959c09f
rexy <ly2352@columbia.edu>
2015-12-04 19:49:33 -0500

    auto license

--- src/jepsen/aurora.clj
+++ src/jepsen/aurora.clj
@@ -27,7 +27,7 @@
    (c/exec :echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" :| :tee :-a "/etc/apt/sources.list.d/webupd8team-java.list")
    (c/exec :apt-key "adv" :--keyserver "hkp://keyserver.ubuntu.com:80" :--recv-keys "EEA14886")
    (c/exec :apt-get "update")
-   (c/exec :echo "oracle-java8-installer" "shared/accepted-oracle-license-v1-1" "select" "true" :| "/usr/bin/debconf-set-selections")
+   (c/exec :echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" :| "/usr/bin/debconf-set-selections")
    (c/exec :apt-get :install "oracle-java8-installer" :-y :--force-yes)
 
    (c/exec :curl :-L "https://github.com/jchli/jepsen-aurora/raw/master/aurora/dist/distributions/aurora-scheduler-0.11.0-SNAPSHOT.zip" :-o "aurora-scheduler.zip")
