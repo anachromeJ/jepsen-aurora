cefc624f1a3ff5ec0150f17f4a8e208ea0f51510
Jincheng Li <jens.vivre@gmail.com>
2015-12-04 21:00:43 -0500

    added unzip option

--- src/jepsen/aurora.clj
+++ src/jepsen/aurora.clj
@@ -31,7 +31,7 @@
    (c/exec :apt-get :install "oracle-java8-installer" :-y :--force-yes)
 
    (c/exec :curl :-L "https://github.com/jchli/jepsen-aurora/raw/master/aurora/dist/distributions/aurora-scheduler-0.11.0-SNAPSHOT.zip" :-o "aurora-scheduler.zip")
-   (c/exec :unzip "aurora-scheduler.zip" :-d "/usr/local")
+   (c/exec :unzip :-n "aurora-scheduler.zip" :-d "/usr/local")
    (c/exec :mv :-f "/usr/local/aurora-scheduler-0.11.0-SNAPSHOT" "/usr/local/aurora-scheduler")))
 ;; (c/exec :ln :-nfs "aurora-scheduler" "/usr/local/aurora-scheduler")))
 
