b4f0b22bcc9b9d440bee6951901d8d948f89f02d
Jincheng Li <jl4569@columbia.edu>
2015-12-10 20:31:44 -0500

    slave resources

--- src/jepsen/mesos.clj
+++ src/jepsen/mesos.clj
@@ -107,7 +107,6 @@
               :--pidfile        slave-pidfile
               :--chdir          slave-dir
               :--exec           slave-bin
-              :--resources      "cpus:10;mem:1000;disk:1000"
               :--no-close
               :--oknodo
               :--
@@ -116,6 +115,7 @@
               (str "--recovery_timeout=30secs")
               (str "--work_dir="  slave-dir)
               (str "--master="    (zk-uri test))
+              (str "--resources=cpus:10;mem:1000;disk:1000")
               :>> (str log-dir "/slave.stdout")
               (c/lit "2>&1")))))
 
