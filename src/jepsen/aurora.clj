abc42aa046a597460480f862b2cb659a5eb44f67
Jincheng Li <jl4569@columbia.edu>
2015-12-18 21:03:47 -0500

    no nemesis

--- src/jepsen/aurora.clj
+++ src/jepsen/aurora.clj
@@ -195,22 +195,22 @@
                      (->> (add-job)
                           (gen/delay 30)
                           (gen/stagger 30)
-                          (gen/nemesis
-                           (gen/seq (cycle [(gen/sleep 200)
-                                            {:type :info, :f :start}
-                                            (gen/sleep 200)
-                                            {:type :info, :f :stop}
-                                            {:type :info, :f :resurrect}])))
+                          ;; (gen/nemesis
+                          ;;  (gen/seq (cycle [(gen/sleep 200)
+                          ;;                   {:type :info, :f :start}
+                          ;;                   (gen/sleep 200)
+                          ;;                   {:type :info, :f :stop}
+                          ;;                   {:type :info, :f :resurrect}])))
                           (gen/time-limit 450))
-                     (gen/nemesis (gen/once {:type :info, :f :stop}))
-                     (gen/nemesis (gen/once {:type :info, :f :resurrect}))
+                     ;; (gen/nemesis (gen/once {:type :info, :f :stop}))
+                     ;; (gen/nemesis (gen/once {:type :info, :f :resurrect}))
                      (gen/log "Waiting for executions")
                      (gen/sleep 400)
                      (gen/clients
                       (gen/once
                        {:type :invoke, :f :read})))
-         :nemesis   (resurrection-hub
-                     (nemesis/partition-random-halves))
+         ;; :nemesis   (resurrection-hub
+         ;;             (nemesis/partition-random-halves))
          :checker   (checker/compose
                      {:aurora (checker)
                       :perf (checker/perf)})
